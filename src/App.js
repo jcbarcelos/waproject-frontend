0f67e95e2f83ff7c662e87a831b6b4cc2991f410
jcbarcelosrj <jcbarcelosrj@gmail.com>
2022-11-26 15:41:40 -0300

    feat: create project and add axios

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+      <h1>Filmes</h1>
+    </div>
+  );
+}
+
+export default App;
