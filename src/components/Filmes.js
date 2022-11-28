519be229ce4b2efed1e1461f3a7f51c715db9d25
jcbarcelosrj <jcbarcelosrj@gmail.com>
2022-11-28 11:51:46 -0300

    refactor: modify filmes

--- src/components/Filmes.js
+++ src/components/Filmes.js
@@ -1,26 +1,34 @@
 import React from 'react';
 
 const Filmes = ({ filmes, loading }) => {
-  if (loading) {
-    return <h2>Loading...</h2>
-  }
-  return (
-    <div className="row">
-      {filmes.map(filme => (
-        <div key={filme.id} className="col-sm-6">
-          <div className="card">
-            <img src={filme.movie_banner} className="card-img-top" alt="Imagem banner" />
-            <div className="card-body">
-              <h5 className="card-title">{filme.title}</h5>
-              <p className="card-text">{filme.descrição}</p>
-              <p className="card-text">{filme.diretor}</p>
-              <p className="card-text">{filme.produtor}</p>
 
+  return (
+    <>
+      <div className="row">
+        {filmes.map((filme, i) => (
+          <div key={filme.id} className="col-sm-6">
+            <div className="card mb-3">
+              <img src={filme.movie_banner} className="card-img-top" alt="Imagem banner" />
+              <div className="card-body" id="description">
+                <h5 className="card-title">{filme.title}</h5>
+                <p className='card-text '>
+                  {filme.description}
+                </p>
+                <span className="card-text">Director: {filme.director}</span>
+                <br />
+                <span className="card-text">Producer: {filme.producer}</span>
+
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))
+        }
+      </div >
+      {loading && (<div class="d-flex spinner-grow text-dark  justify-content-center" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>)}
+
+    </>
 
   );
 }
